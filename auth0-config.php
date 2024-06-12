4b634d7d25557648b04cf67d5d59dc1506fbb4b3
FAYSi223 <134807221+FAYSi223@users.noreply.github.com>
2024-06-12 11:46:10 +0200

    Create auth0-config.php

---
+++ auth0-config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'domain' => 'social-faysi.eu.auth0.com',
+    'client_id' => 'MPT9y6ICHRcyNRGYVR3JPYM4uVrKwDsI',
+    'client_secret' => 'xs1-9qdtWH8mlZO_S86jqhAQ5O-jomd49p9JMVGCg5Z9owlA9rB612Hl7uqQ9xlG',
+    'redirect_uri' => 'https://social.faysi.de/callback.php',
+    'audience' => 'https://social-faysi.eu.auth0.com/api/v2/',
+    'scope' => 'openid profile email',
+];
